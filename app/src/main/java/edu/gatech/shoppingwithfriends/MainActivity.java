3918c891ccf5107779a814615e4f60dde3133c05
yxjames <zhugeyx@163.com>
2015-01-29 16:53:51 -0500

    Login layout and string_activity_login.xml

--- app/src/main/java/edu/gatech/shoppingwithfriends/MainActivity.java
+++ app/src/main/java/edu/gatech/shoppingwithfriends/MainActivity.java
@@ -1,19 +1,36 @@
 package edu.gatech.shoppingwithfriends;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    Button loginPageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginPageButton = (Button)findViewById(R.id.button2);
+        loginPageButton.setOnClickListener(this);
     }
-
+    public void button2Click(){
+        startActivity(new Intent("ShoppingWithFriends.LoginActivity"));
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                button2Click();
+                break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
